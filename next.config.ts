182a1adeae684235b0b2dbfbb908f592f8921e70
Carlos Gallegos <carlosalbrtohg@gmail.com>
2024-11-27 12:52:12 -0600

    Delete middleware

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Modo estricto de React
-  trailingSlash: false, // URLs sin barra final
-  output: 'standalone', // Portabilidad mejorada
-};
-
-export default nextConfig;
+    reactStrictMode: true, // Modo estricto de React
+    trailingSlash: false, // URLs sin barra final
+    output: 'standalone', // Portabilidad mejorada
+  };
+  
+ export default nextConfig;
