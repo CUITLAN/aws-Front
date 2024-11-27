a292643a4fc420d15f33c9ecb5a8427bbb5b9d36
Carlos Gallegos <carlosalbrtohg@gmail.com>
2024-11-27 11:01:59 -0600

    Moified files

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true, // Modo estricto de React
+    trailingSlash: false, // URLs sin barra final
+    output: 'standalone', // Portabilidad mejorada
+  };
+  
+  export default nextConfig;
+  
